808302e08827cea7a85dbcbdd4add1067642e991
Anushakv123 <anushakv1323@gmail.com>
2024-12-18 16:36:17 +0530

    home page updated

--- e-commerce/src/Components/Header.jsx
+++ e-commerce/src/Components/Header.jsx
@@ -5,7 +5,7 @@ import './Header.css'
 function Header() {
   return (
     <header className='header'>
-        <h1>GroceryJem</h1>
+        <h1>Zuko</h1>
         <button className='logout_button'>Log out</button>
     </header>
   )
