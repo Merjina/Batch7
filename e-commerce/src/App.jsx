701a42b9581f4409fd82007d7a9ffc7cd80d66a6
Anushakv123 <anushakv1323@gmail.com>
2024-12-18 13:00:52 +0530

    home page updated

--- e-commerce/src/App.jsx
+++ e-commerce/src/App.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import Home from "./Components/Home";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 function App() {
   return (
-    <div className="App">
-     <p>react project</p>
+    <div>
+      <Header/>
+      <Home/>
+      <Footer/>
     </div>
-  );
+  )
 }
 
 export default App;
