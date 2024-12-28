a654b17fd463522b6f71069267b0e8c0469cd33c
ANANYA1234K <ananyapraveenanu@gmail.com>
2024-12-28 11:26:10 +0530

    merge commit

--- e-commerce/src/components/Navbar.jsx
+++ e-commerce/src/components/Navbar.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Navbar.css';
-import { Link } from 'react-router-dom';
 
 const Navbar = ({ setCategory, cartItems }) => {
   // Calculate total items in the cart
